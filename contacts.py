493f8d5c5742a3c19e091272789104405e1e8b99
AryaNaidu747 <89949622+AryaNaidu747@users.noreply.github.com>
2026-05-19 16:04:29 -0400

    Update contacts.py
    Debugged the start of the code, option 1 and option 3 (and option 5) work now. Still need to figure out how to make it so that phone and email are in correct format and create option 3 and option 4.

--- contacts.py
+++ contacts.py
@@ -6,6 +6,7 @@ Each contact stores:
 Name, Phone number, Email
 '''
 
+'''
 def add_contact():
 
 def rem_contact():
@@ -13,18 +14,20 @@ def rem_contact():
 def view_contact():
 
 def delete_contact():
+'''
 
 contacts = []
 while True: 
   starting_input =  input("Enter 1 for new contact, 2 to search contact, 3 to view contact, 4 to delete contact, 5 to quit: ")
-  if starting_input == 1:
-    name = input("Enter a name").strip().title()
-    phone = input("Enter a phone number in format XXX-XXX-XXXX")
-    email = input("Enter an email in format email@address.xxx")
+  if starting_input == "1":
+    name = input("Enter a name: ").strip().title()
+    phone = input("Enter a phone number in format XXX-XXX-XXXX: ")
+    email = input("Enter an email in format email@address.xxx: ")
     contact = {"name": name, "phone": phone, "email": email}
     contacts.append(contact)
-  if starting_input == 2:
+  if starting_input == "3":
     for contact in contacts:
-      print(contact["name"]
-  if starting_input == 5:
-      return
+      print(f"{contact["name"]}, {contact["phone"]}, {contact["email"]}")
+      
+  if starting_input == "5":
+      break
