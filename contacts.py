4159a2bf85333abe839abc314e3268da259c8453
AryaNaidu747 <89949622+AryaNaidu747@users.noreply.github.com>
2026-05-19 15:42:05 -0400

    Create contacts.py

---
+++ contacts.py
@@ -0,0 +1,7 @@
+'''
+Mini Project: Command-Line Contact Book
+User can:
+Add contacts, Search contacts, View all contacts, Delete contacts, Quit
+Each contact stores:
+Name, Phone number, Email
+'''
