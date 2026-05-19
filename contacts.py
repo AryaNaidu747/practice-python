d89da96824703a5e9362fd550067864e21e70632
AryaNaidu747 <89949622+AryaNaidu747@users.noreply.github.com>
2026-05-19 15:54:08 -0400

    Update contacts.py

--- contacts.py
+++ contacts.py
@@ -5,3 +5,26 @@ Add contacts, Search contacts, View all contacts, Delete contacts, Quit
 Each contact stores:
 Name, Phone number, Email
 '''
+
+def add_contact():
+
+def rem_contact():
+
+def view_contact():
+
+def delete_contact():
+
+contacts = []
+while True: 
+  starting_input =  input("Enter 1 for new contact, 2 to search contact, 3 to view contact, 4 to delete contact, 5 to quit: ")
+  if starting_input == 1:
+    name = input("Enter a name").strip().title()
+    phone = input("Enter a phone number in format XXX-XXX-XXXX")
+    email = input("Enter an email in format email@address.xxx")
+    contact = {"name": name, "phone": phone, "email": email}
+    contacts.append(contact)
+  if starting_input == 2:
+    for contact in contacts:
+      print(contact["name"]
+  if starting_input == 5:
+      return
