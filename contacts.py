2aec4b6b78a28e61c9b20a44a33202ff2550c09c
AryaNaidu747 <89949622+AryaNaidu747@users.noreply.github.com>
2026-05-28 18:40:08 -0400

    Update contacts.py

--- contacts.py
+++ contacts.py
@@ -6,32 +6,55 @@ Each contact stores:
 Name, Phone number, Email
 '''
 
-'''
-def add_contact():
-
-def rem_contact():
-
-def view_contact():
-
-def delete_contact():
-'''
-
 contacts = []
+
 while True: 
-  starting_input =  input("Enter 1 for new contact, 2 to search contact, 3 to view contact, 4 to delete contact, 5 to quit: ")
+  starting_input = input("Enter 1 for new contact, 2 to search contact, 3 to view contact, 4 to delete contact, 5 to quit: ")
+
   if starting_input == "1":
     name = input("Enter a name: ").strip().title()
     phone = input("Enter a phone number in format XXX-XXX-XXXX: ")
     email = input("Enter an email in format email@address.xxx: ")
+
     contact = {"name": name, "phone": phone, "email": email}
     contacts.append(contact)
-  if starting_input == "3":
-    for contact in contacts:
-      print(f"{contact["name"]}, {contact["phone"]}, {contact["email"]}")
+
+    print("Contact added")
+
   if starting_input == "2":
-    break
-  if starting_inpit == "4":
-    break
-      
+    search_name = input("Enter contact name: ").strip().title()
+
+    found = False
+
+    for contact in contacts:
+      if contact["name"] == search_name:
+        print(f'{contact["name"]}, {contact["phone"]}, {contact["email"]}')
+        found = True
+
+    if found == False:
+      print("Contact not found")
+
+  if starting_input == "3":
+    if len(contacts) == 0:
+      print("No contacts")
+
+    for contact in contacts:
+      print(f'{contact["name"]}, {contact["phone"]}, {contact["email"]}')
+
+  if starting_input == "4":
+    delete_name = input("Enter contact name to delete: ").strip().title()
+
+    found = False
+
+    for contact in contacts:
+      if contact["name"] == delete_name:
+        contacts.remove(contact)
+        print("Contact deleted")
+        found = True
+        break
+
+    if found == False:
+      print("Contact not found")
+
   if starting_input == "5":
-      break
+    break
